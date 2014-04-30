e82806262d04ce346469f76c5c82a5ecf925feba
Elia Grady <eliagrady19@gmail.com>
2014-04-30 15:23:07 +0300

    fixed settings display, no logic attached yet - suggested DataContainerJSON.js added

--- src/main/webapp/static/js/DataContainerJSON.js
+++ src/main/webapp/static/js/DataContainerJSON.js
@@ -4,37 +4,45 @@
 
 
 var appSettings = {
-    cm : {
+    engine : {
+        configuration : {},
         settings : {}
-    }
-
-
-
-
-}
+    },
+    projectsMetaData : [
+        "project1Obj",
+        "project2Obj",
+        "project3Obj"
+    ]
+};
 
 
 var project = {
+    id : "projectId-in-some-format",
+    name : "String",
+    description : "@Nullable String",
+    revision : {
+        id : 1,
+        date : "Date in some format"
+    },
+    type: "Project type {JavaScript, MixedHTML, some other modules}"
 
-}
+};
 
 var appData = {
-    projectsMetaData : [],
-    projectsData : {
-        projectId : {
-            name : "project name",
-            revision : {
-                id : 1,
-                date : "revision date"
-            },
-            description : "project description and\or notes",
-            code : {
-                html : {},
-                js : {},
-                css : {}
-            }
-        }
-
+    projectId1 : {
+        html : {},
+        js : {},
+        css : {}
+    },
+    projectId1 : {
+        html : {},
+        js : {},
+        css : {}
+    },
+    projectId1 : {
+        html : {},
+        js : {},
+        css : {}
     }
 }
 
