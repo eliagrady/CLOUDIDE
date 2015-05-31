4d347455e8a59d2f3d18de4d3f3678deaeed3f57
Elia Grady <eliagrady19@gmail.com>
2015-05-31 19:50:44 +0300

    migrating to github

--- src/main/java/com/wixpress/app/domain/AppKeys.java
+++
@@ -1,9 +0,0 @@
-package com.wixpress.app.domain;
-
-/**
- * Created by Elia on 31/05/2015.
- */
-public class AppKeys {
-    public final static String APPLICATION_KEY = "13469a7d-eba1-4005-0f59-f2ad9f0bdace";
-    public final static String APPLICATION_SECRET_KEY = "e41683c4-1168-422c-83ef-31eeccc38869";
-}
